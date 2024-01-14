5dda8f85b42a6f5db2c4f09fd4f4f9a9e701122a
Lara Newsom <laramnewsom@gmail.com>
2024-01-14 00:47:45 -0600

    managed focus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { filter } from 'rxjs/operators';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -16,4 +18,18 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  constructor(
+    private readonly router: Router
+  ) {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      takeUntilDestroyed()
+    ).subscribe(() => {
+        const content = document.querySelector<HTMLElement>('#mainRouter');
+        if(content){
+          content.focus();
+        }
+      }
+    )
+  }
 }
--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, inject } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { SideMenuComponent } from './side-menu/side-menu.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -19,6 +21,21 @@ export class ProductViewComponent{
   }
 
   protected readonly productService = inject(ProductService);
+
+  constructor(
+    private readonly router: Router
+  ) {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      takeUntilDestroyed()
+    ).subscribe(() => {
+        const content = document.querySelector<HTMLElement>('#productDetail');
+        if(content){
+          content.focus();
+        }
+      }
+    )
+  }
 }
 
 
