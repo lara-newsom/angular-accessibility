b3baf889a010a47b85e181746850a4be88126952
Lara Newsom <laramnewsom@gmail.com>
2024-01-14 00:10:36 -0600

    fixed routing issue

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,4 +1,3 @@
-
 export enum Category {
   GLASSES = 'glasses',
   HATS = 'hats',
@@ -10,27 +9,33 @@ export enum Category {
 export interface CategoryLink {
   category: Category;
   description: string;
+  firstProduct?: string;
 }
 
 export const LINKS: CategoryLink[] = [
   {
     category: Category.HATS,
     description: 'Animals in Hats',
+    firstProduct: 'bird-in-straw-hat'
   },
   {
     category: Category.GLASSES,
     description: 'Animals in Glasses',
+    firstProduct: 'cat-glasses-1'
   },
   {
     category: Category.CLOTHES,
     description: 'Animals in Clothes',
+    firstProduct: 'banana-puppy'
   },
   {
     category: Category.OTHER,
     description: 'Animals Accessorizing',
+    firstProduct: 'bowtie-dog'
   },
   {
     category: Category.ALL,
     description: 'Animals Doing it All',
+    firstProduct: 'banana-puppy'
   },
 ]
